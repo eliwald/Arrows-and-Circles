74ed43ef0677ed1bff506eb4c34bca37deaf9da1
Elias Wald <elias_wald@brown.edu>
2012-05-07 15:53:49 -0400

    minor changes

--- src/frontend/MainFrame.java
+++ src/frontend/MainFrame.java
@@ -1373,16 +1373,32 @@ public class MainFrame extends javax.swing.JFrame {
 		_nodeDragged = n;
 	}
 	
+	public Node getNodeDragged() {
+		return _nodeDragged;
+	}
+	
 	public void setResizing(Node n){
 		_resizing = n;
 	}
 	
+	public Node getResizing() {
+		return _resizing;
+	}
+	
 	public void setEdgeDragged(Edge e) {
 		_edgeDragged = e;
+	}	
+	
+	public Edge getEdgeDragged() {
+		return _edgeDragged;
 	}
 	
 	public void setSelectPoint(Point p) {
 		_selectPoint = p;
+	}
+	
+	public Point getSelectPoint() {
+		return _selectPoint;
 	}
 	
 	public EdgeDirection getEdgeType() {
@@ -1397,24 +1413,8 @@ public class MainFrame extends javax.swing.JFrame {
 		return _mouseLoc;
 	}
 	
-	public Point getSelectPoint() {
-		return _selectPoint;
-	}
-	
-	public Node getNodeDragged() {
-		return _nodeDragged;
-	}
-	
 	public Collection<Node> getNodesSelected() {
 		return _nodesSelected;
-	}
-	
-	public Node getResizing() {
-		return _resizing;
-	}
-	
-	public Edge getEdgeDragged() {
-		return _edgeDragged;
 	}
 	
 	public Collection<Edge> getEdgesSelected() {
